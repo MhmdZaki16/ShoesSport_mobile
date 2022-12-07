c5bb2a2e64e90240adf3c6183d9fa9ca1a4bc5fd
MhmdZaki16 <muhammadzaki9786@gmail.com>
2022-12-07 20:45:24 +0700

    First Comit

---
+++ lib/main.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/shoessportapp/generatednikewidget/GeneratedNikeWidget.dart';
+import 'package:flutterapp/shoessportapp/generatedkeranjang2widget/GeneratedKeranjang2Widget.dart';
+import 'package:flutterapp/shoessportapp/generatedpemesanan2widget/GeneratedPemesanan2Widget.dart';
+import 'package:flutterapp/shoessportapp/generateddonewidget/GeneratedDoneWidget.dart';
+import 'package:flutterapp/shoessportapp/generateddonewidget1/GeneratedDoneWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedprofilwidget/GeneratedProfilWidget.dart';
+import 'package:flutterapp/shoessportapp/generatednotifikasiwidget/GeneratedNotifikasiWidget.dart';
+import 'package:flutterapp/shoessportapp/generatedpesanansayawidget/GeneratedPesananSayaWidget.dart';
+import 'package:flutterapp/shoessportapp/generatedspesialofferswidget/GeneratedSpesialOffersWidget.dart';
+import 'package:flutterapp/shoessportapp/generatedprofilwidget2/GeneratedProfilWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedhomepageswidget/GeneratedHomePagesWidget.dart';
+import 'package:flutterapp/shoessportapp/generatedsignupwidget2/GeneratedSignUpWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedsigninwidget2/GeneratedSignInWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedtextwidget/GeneratedTextWidget.dart';
+import 'package:flutterapp/shoessportapp/generatednamawidget1/GeneratedNamaWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedakunwidget1/GeneratedAkunWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedteleponwidget2/GeneratedTeleponWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedemailwidget2/GeneratedEmailWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedalamatwidget3/GeneratedAlamatWidget3.dart';
+import 'package:flutterapp/shoessportapp/generatedcode2widget1/GeneratedCode2Widget1.dart';
+import 'package:flutterapp/shoessportapp/generatedhargawidget1/GeneratedHargaWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedmewakiliwidget/GeneratedMewakiliWidget.dart';
+import 'package:flutterapp/shoessportapp/generatedpenambahanwidget/GeneratedPenambahanWidget.dart';
+import 'package:flutterapp/shoessportapp/generatedsize43widget1/GeneratedSize43Widget1.dart';
+import 'package:flutterapp/shoessportapp/generatedsize42widget1/GeneratedSize42Widget1.dart';
+import 'package:flutterapp/shoessportapp/generatedpesanansayawidget2/GeneratedPesananSayaWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedalamatwidget4/GeneratedAlamatWidget4.dart';
+import 'package:flutterapp/shoessportapp/generateddanawidget2/GeneratedDanaWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedgopaywidget2/GeneratedGopayWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedqriswidget2/GeneratedQrisWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedovowidget1/GeneratedOvoWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedlinkajawidget2/GeneratedLinkajaWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedcentangwidget1/GeneratedCentangWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedcentang2widget2/GeneratedCentang2Widget2.dart';
+import 'package:flutterapp/shoessportapp/generatedhalamanlogindaftarwidget/GeneratedHalamanLogindaftarWidget.dart';
+import 'package:flutterapp/shoessportapp/generatednikewidget6/GeneratedNikeWidget6.dart';
+import 'package:flutterapp/shoessportapp/generatedhomepageswidget1/GeneratedHomePagesWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedspesialofferswidget1/GeneratedSpesialOffersWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedsignupwidget5/GeneratedSignUpWidget5.dart';
+import 'package:flutterapp/shoessportapp/generatedsigninwidget5/GeneratedSignInWidget5.dart';
+import 'package:flutterapp/shoessportapp/generatedcode2widget3/GeneratedCode2Widget3.dart';
+import 'package:flutterapp/shoessportapp/generatedcode2widget4/GeneratedCode2Widget4.dart';
+import 'package:flutterapp/shoessportapp/generatedloginwidget/GeneratedLoginWidget.dart';
+import 'package:flutterapp/shoessportapp/generateddasboardwidget/GeneratedDasboardWidget.dart';
+import 'package:flutterapp/shoessportapp/generatedbarangwidget1/GeneratedBarangWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedtransaksiwidget2/GeneratedTransaksiWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedprofilwidget6/GeneratedProfilWidget6.dart';
+import 'package:flutterapp/shoessportapp/generatedkategoriwidget5/GeneratedKategoriWidget5.dart';
+import 'package:flutterapp/shoessportapp/generatedhomepageswidget2/GeneratedHomePagesWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedhomepageswidget3/GeneratedHomePagesWidget3.dart';
+import 'package:flutterapp/shoessportapp/generatedprodukwidget/GeneratedProdukWidget.dart';
+import 'package:flutterapp/shoessportapp/generatedprofilwidget9/GeneratedProfilWidget9.dart';
+import 'package:flutterapp/shoessportapp/generatednotifikasiwidget3/GeneratedNotifikasiWidget3.dart';
+import 'package:flutterapp/shoessportapp/generatedpesanansayawidget2/GeneratedPesanansayaWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedbelumbayarwidget3/GeneratedBelumBayarWidget3.dart';
+import 'package:flutterapp/shoessportapp/generateddonewidget2/GeneratedDoneWidget2.dart';
+import 'package:flutterapp/shoessportapp/generatedcode2widget6/GeneratedCode2Widget6.dart';
+import 'package:flutterapp/shoessportapp/generatedlogin2widget1/GeneratedLogin2Widget1.dart';
+import 'package:flutterapp/shoessportapp/generateddaftar2widget1/GeneratedDaftar2Widget1.dart';
+import 'package:flutterapp/shoessportapp/generatedlogin3widget1/GeneratedLogin3Widget1.dart';
+import 'package:flutterapp/shoessportapp/generatedsizewidget4/GeneratedSizeWidget4.dart';
+import 'package:flutterapp/shoessportapp/generatedalamatwebwidget1/GeneratedAlamatWebWidget1.dart';
+import 'package:flutterapp/shoessportapp/generateddanawebwidget1/GeneratedDanaWebWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedgopaywebwidget1/GeneratedGopaywebWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedlinkajawebwidget1/GeneratedLinkajawebWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedovowebwidget1/GeneratedOvoWebWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedqriswebwidget1/GeneratedQrisWebWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedalfawebwidget1/GeneratedAlfawebWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedindomaretwebwidget1/GeneratedIndomaretWebWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedbankwebwidget1/GeneratedBankWebWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedhargawidget4/GeneratedHargaWidget4.dart';
+import 'package:flutterapp/shoessportapp/generatedcentangwebwidget1/GeneratedCentangwebWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedloginanddaftarwidget/GeneratedLoginandDaftarWidget.dart';
+import 'package:flutterapp/shoessportapp/generatedhomepageswidget4/GeneratedHomePagesWidget4.dart';
+import 'package:flutterapp/shoessportapp/generatedsizewidget5/GeneratedSizeWidget5.dart';
+import 'package:flutterapp/shoessportapp/generatedprodukwidget1/GeneratedProdukWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedcode2widget9/GeneratedCode2Widget9.dart';
+import 'package:flutterapp/shoessportapp/generateddaftar2widget3/GeneratedDaftar2Widget3.dart';
+import 'package:flutterapp/shoessportapp/generateddaftar2widget4/GeneratedDaftar2Widget4.dart';
+import 'package:flutterapp/shoessportapp/generatedlogin3widget3/GeneratedLogin3Widget3.dart';
+import 'package:flutterapp/shoessportapp/generatedlogin3widget4/GeneratedLogin3Widget4.dart';
+import 'package:flutterapp/shoessportapp/generatedloginwidget10/GeneratedLoginWidget10.dart';
+import 'package:flutterapp/shoessportapp/generateddasboardwidget1/GeneratedDasboardWidget1.dart';
+import 'package:flutterapp/shoessportapp/generatedbarangwidget7/GeneratedBarangWidget7.dart';
+import 'package:flutterapp/shoessportapp/generatedtransaksiwidget8/GeneratedTransaksiWidget8.dart';
+import 'package:flutterapp/shoessportapp/generatedprofilwidget13/GeneratedProfilWidget13.dart';
+import 'package:flutterapp/shoessportapp/generatedkategoriwidget17/GeneratedKategoriWidget17.dart';
+
+void main() {
+  runApp(ShoesSportApp());
+}
+
+class ShoesSportApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedProfilWidget2',
+      routes: {
+        '/GeneratedNikeWidget': (context) => GeneratedNikeWidget(),
+        '/GeneratedKeranjang2Widget': (context) => GeneratedKeranjang2Widget(),
+        '/GeneratedPemesanan2Widget': (context) => GeneratedPemesanan2Widget(),
+        '/GeneratedDoneWidget': (context) => GeneratedDoneWidget(),
+        '/GeneratedDoneWidget1': (context) => GeneratedDoneWidget1(),
+        '/GeneratedProfilWidget': (context) => GeneratedProfilWidget(),
+        '/GeneratedNotifikasiWidget': (context) => GeneratedNotifikasiWidget(),
+        '/GeneratedPesananSayaWidget': (context) =>
+            GeneratedPesananSayaWidget(),
+        '/GeneratedSpesialOffersWidget': (context) =>
+            GeneratedSpesialOffersWidget(),
+        '/GeneratedProfilWidget2': (context) => GeneratedProfilWidget2(),
+        '/GeneratedHomePagesWidget': (context) => GeneratedHomePagesWidget(),
+        '/GeneratedSignUpWidget2': (context) => GeneratedSignUpWidget2(),
+        '/GeneratedSignInWidget2': (context) => GeneratedSignInWidget2(),
+        '/GeneratedTextWidget': (context) => GeneratedTextWidget(),
+        '/GeneratedNamaWidget1': (context) => GeneratedNamaWidget1(),
+        '/GeneratedAkunWidget1': (context) => GeneratedAkunWidget1(),
+        '/GeneratedTeleponWidget2': (context) => GeneratedTeleponWidget2(),
+        '/GeneratedEmailWidget2': (context) => GeneratedEmailWidget2(),
+        '/GeneratedAlamatWidget3': (context) => GeneratedAlamatWidget3(),
+        '/GeneratedCode2Widget1': (context) => GeneratedCode2Widget1(),
+        '/GeneratedHargaWidget1': (context) => GeneratedHargaWidget1(),
+        '/GeneratedMewakiliWidget': (context) => GeneratedMewakiliWidget(),
+        '/GeneratedPenambahanWidget': (context) => GeneratedPenambahanWidget(),
+        '/GeneratedSize43Widget1': (context) => GeneratedSize43Widget1(),
+        '/GeneratedSize42Widget1': (context) => GeneratedSize42Widget1(),
+        '/GeneratedPesananSayaWidget2': (context) =>
+            GeneratedPesananSayaWidget2(),
+        '/GeneratedAlamatWidget4': (context) => GeneratedAlamatWidget4(),
+        '/GeneratedDanaWidget2': (context) => GeneratedDanaWidget2(),
+        '/GeneratedGopayWidget2': (context) => GeneratedGopayWidget2(),
+        '/GeneratedQrisWidget2': (context) => GeneratedQrisWidget2(),
+        '/GeneratedOvoWidget1': (context) => GeneratedOvoWidget1(),
+        '/GeneratedLinkajaWidget2': (context) => GeneratedLinkajaWidget2(),
+        '/GeneratedCentangWidget1': (context) => GeneratedCentangWidget1(),
+        '/GeneratedCentang2Widget2': (context) => GeneratedCentang2Widget2(),
+        '/GeneratedHalamanLogindaftarWidget': (context) =>
+            GeneratedHalamanLogindaftarWidget(),
+        '/GeneratedNikeWidget6': (context) => GeneratedNikeWidget6(),
+        '/GeneratedHomePagesWidget1': (context) => GeneratedHomePagesWidget1(),
+        '/GeneratedSpesialOffersWidget1': (context) =>
+            GeneratedSpesialOffersWidget1(),
+        '/GeneratedSignUpWidget5': (context) => GeneratedSignUpWidget5(),
+        '/GeneratedSignInWidget5': (context) => GeneratedSignInWidget5(),
+        '/GeneratedCode2Widget3': (context) => GeneratedCode2Widget3(),
+        '/GeneratedCode2Widget4': (context) => GeneratedCode2Widget4(),
+        '/GeneratedLoginWidget': (context) => GeneratedLoginWidget(),
+        '/GeneratedDasboardWidget': (context) => GeneratedDasboardWidget(),
+        '/GeneratedBarangWidget1': (context) => GeneratedBarangWidget1(),
+        '/GeneratedTransaksiWidget2': (context) => GeneratedTransaksiWidget2(),
+        '/GeneratedProfilWidget6': (context) => GeneratedProfilWidget6(),
+        '/GeneratedKategoriWidget5': (context) => GeneratedKategoriWidget5(),
+        '/GeneratedHomePagesWidget2': (context) => GeneratedHomePagesWidget2(),
+        '/GeneratedHomePagesWidget3': (context) => GeneratedHomePagesWidget3(),
+        '/GeneratedProdukWidget': (context) => GeneratedProdukWidget(),
+        '/GeneratedProfilWidget9': (context) => GeneratedProfilWidget9(),
+        '/GeneratedNotifikasiWidget3': (context) =>
+            GeneratedNotifikasiWidget3(),
+        '/GeneratedPesanansayaWidget2': (context) =>
+            GeneratedPesanansayaWidget2(),
+        '/GeneratedBelumBayarWidget3': (context) =>
+            GeneratedBelumBayarWidget3(),
+        '/GeneratedDoneWidget2': (context) => GeneratedDoneWidget2(),
+        '/GeneratedCode2Widget6': (context) => GeneratedCode2Widget6(),
+        '/GeneratedLogin2Widget1': (context) => GeneratedLogin2Widget1(),
+        '/GeneratedDaftar2Widget1': (context) => GeneratedDaftar2Widget1(),
+        '/GeneratedLogin3Widget1': (context) => GeneratedLogin3Widget1(),
+        '/GeneratedSizeWidget4': (context) => GeneratedSizeWidget4(),
+        '/GeneratedAlamatWebWidget1': (context) => GeneratedAlamatWebWidget1(),
+        '/GeneratedDanaWebWidget1': (context) => GeneratedDanaWebWidget1(),
+        '/GeneratedGopaywebWidget1': (context) => GeneratedGopaywebWidget1(),
+        '/GeneratedLinkajawebWidget1': (context) =>
+            GeneratedLinkajawebWidget1(),
+        '/GeneratedOvoWebWidget1': (context) => GeneratedOvoWebWidget1(),
+        '/GeneratedQrisWebWidget1': (context) => GeneratedQrisWebWidget1(),
+        '/GeneratedAlfawebWidget1': (context) => GeneratedAlfawebWidget1(),
+        '/GeneratedIndomaretWebWidget1': (context) =>
+            GeneratedIndomaretWebWidget1(),
+        '/GeneratedBankWebWidget1': (context) => GeneratedBankWebWidget1(),
+        '/GeneratedHargaWidget4': (context) => GeneratedHargaWidget4(),
+        '/GeneratedCentangwebWidget1': (context) =>
+            GeneratedCentangwebWidget1(),
+        '/GeneratedLoginandDaftarWidget': (context) =>
+            GeneratedLoginandDaftarWidget(),
+        '/GeneratedHomePagesWidget4': (context) => GeneratedHomePagesWidget4(),
+        '/GeneratedSizeWidget5': (context) => GeneratedSizeWidget5(),
+        '/GeneratedProdukWidget1': (context) => GeneratedProdukWidget1(),
+        '/GeneratedCode2Widget9': (context) => GeneratedCode2Widget9(),
+        '/GeneratedDaftar2Widget3': (context) => GeneratedDaftar2Widget3(),
+        '/GeneratedDaftar2Widget4': (context) => GeneratedDaftar2Widget4(),
+        '/GeneratedLogin3Widget3': (context) => GeneratedLogin3Widget3(),
+        '/GeneratedLogin3Widget4': (context) => GeneratedLogin3Widget4(),
+        '/GeneratedLoginWidget10': (context) => GeneratedLoginWidget10(),
+        '/GeneratedDasboardWidget1': (context) => GeneratedDasboardWidget1(),
+        '/GeneratedBarangWidget7': (context) => GeneratedBarangWidget7(),
+        '/GeneratedTransaksiWidget8': (context) => GeneratedTransaksiWidget8(),
+        '/GeneratedProfilWidget13': (context) => GeneratedProfilWidget13(),
+        '/GeneratedKategoriWidget17': (context) => GeneratedKategoriWidget17(),
+      },
+    );
+  }
+}
